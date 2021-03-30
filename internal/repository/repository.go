29d89c6e5059d8703cec72b12c4357c72e4381af
Владислав <dreamweaver3x@gmail.com>
2021-03-30 19:41:41 +0300

    added error handling

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -17,7 +17,7 @@ func NewLinksRepository(db *gorm.DB) *LinksRepository {
 	return &LinksRepository{db: db}
 }
 
-func (l *LinksRepository) Create(u *models.Link)  error {
+func (l *LinksRepository) Create(u *models.Link) error {
 	tx := l.db.Begin()
 	result := tx.Create(u)
 	if result.Error != nil {
@@ -37,46 +37,42 @@ func (l *LinksRepository) Create(u *models.Link)  error {
 		return result.Error
 	}
 
-	return  nil
+	return nil
 }
 
-func (l *LinksRepository) GetLongUrl(u *models.Link)  error {
+func (l *LinksRepository) GetLongUrl(u *models.Link) error {
 	result := l.db.Where("short_url = ?", u.ShortUrl).First(u)
 	if result.Error != nil {
-		return  result.Error
+		return result.Error
 	}
 	result = l.db.Model(u).Where("short_url = ?", u.ShortUrl).Update("nums_of_redirects", gorm.Expr("nums_of_redirects + ?", 1))
 	if result.Error != nil {
-		return  result.Error
+		return result.Error
 	}
 	return nil
 }
-func (l *LinksRepository) GetFiveHundredLinks(id uint, ch *chan models.Link)  error {
-links := make([]models.Link, 0)
-result := l.db.Select("id","full_url", "accessible").Where("id BETWEEN ? AND ?", id*500+1, id*500+500).Find(&links)
-log.Println("id raven ", id)
-if result.Error != nil {
-	log.Println(result.Error)
-	return result.Error
-}
-if result.RowsAffected == 0 {
-	return fmt.Errorf("net strok na id=%d", id)
-}
-wg := sync.WaitGroup{}
-for i := 0; i < len(links); i++ {
-	wg.Add(1)
-	go func(i int) {
-		defer wg.Done()
-		*ch <- links[i]
-	}(i)
-}
-wg.Wait()
+func (l *LinksRepository) GetFiveHundredLinks(id uint, ch *chan models.Link) error {
+	links := make([]models.Link, 0)
+	result := l.db.Select("id", "full_url", "accessible").Where("id BETWEEN ? AND ?", id*500+1, id*500+500).Find(&links)
+	if result.Error != nil {
+		log.Println(result.Error)
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("no more urls after %d iteration", id)
+	}
+	wg := sync.WaitGroup{}
+	for i := 0; i < len(links); i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			*ch <- links[i]
+		}(i)
+	}
+	wg.Wait()
 
-return nil
+	return nil
 }
-
-
-
 
 func (l *LinksRepository) UpdateAccess(inaccessibleLink, accessibleLink []uint) error {
 	u := models.Link{}
@@ -91,12 +87,10 @@ func (l *LinksRepository) UpdateAccess(inaccessibleLink, accessibleLink []uint) 
 	return nil
 }
 
-func (l *LinksRepository) GetStats(u *models.Link)  error {
+func (l *LinksRepository) GetStats(u *models.Link) error {
 	result := l.db.Where("short_url = ?", u.ShortUrl).First(&u)
 	if result.Error != nil {
 		return result.Error
 	}
 	return nil
 }
-
-
